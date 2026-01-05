5f16f704f3ca308331e8ea4508fbf4f1f973fea3
Andika Agung <75003172+atriadha99@users.noreply.github.com>
2026-01-05 17:17:19 +0700

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portal-ekskul-binusa/", 
+  base: '/', // Ubah jadi '/' atau hapus baris ini sepenuhnya
 })
